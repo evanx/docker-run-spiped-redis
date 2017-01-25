e6a2e6f0d4c26967346d3c99865f5e57447ca1da
Evan Summers <evan.summers@gmail.com>
2017-01-25 09:10:57 +0000

    initial

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,11 +1,14 @@
 
   mkdir -p tmp
   cp app/*js tmp/.
+  redis-cli del restart:adv
+  node --harmony tmp/index.js
   while true
   do
     name=`redis-cli brpop restart:adv 10`
     if [ -n "$name"]
     then
+      echo "$name"
       date +%T
       head tmp/index.js
       node -v
